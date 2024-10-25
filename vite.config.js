509ce155d8bfc9585aa32a54c6869b2d65a16eb6
yangjun1453 <1453195225@qq.com>
2024-10-25 23:29:40 +0800

    change

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),VueSetupExtend()],
+  base:
+  process.env.NODE_ENV === "production"
+    ? "https://yangjun1453.github.io/vampire/"
+    : "/",
 })
