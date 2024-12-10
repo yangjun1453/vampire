8355b90340a5fd77ad85d9854391eab41e921162
yangjun1453 <1453195225@qq.com>
2024-12-10 22:48:25 +0800

    change

--- src/pages/Page2.vue
+++ src/pages/Page2.vue
@@ -1,6 +1,6 @@
 <template>
 <div >
- <Town/>
+Minha força vem do fato de que seu amor me fez acreditar que eu tinha a capacidade de amar, mas na verdade eu não tenho. Eu depositei todo o carinho que acumulei ao longo da vida em você, e isso é muito, o que acaba criando muita pressão sobre você. A situação do passaporte me fez lembrar da minha infância, quando eu tinha aulas de música. A professora me repreendeu por não olhar na direção dela, e naquela época, a única coisa que eu podia fazer era sorrir, porque me sentia perdido. Quando não sabia o que fazer, eu só conseguia rir. Aquela experiência na aula de música me fez me fechar ainda mais. Eu sorri muito pouco depois disso. Você também me fez perceber algo importante: eu nasci de um parto difícil. Minha cabeça era grande e, quando nasci, minha cabeça estava roxa. Deus parecia não querer que eu viesse para esse mundo. Eu deveria me sentir grato por tudo o que tenho, mas sempre que penso nisso, sinto uma grande tristeza. Quando estou na aula à noite, não consigo segurar as lágrimas. Eu me sinto muito triste.
 </div>
 </template>
 
--- src/pages/Page4.vue
+++ src/pages/Page4.vue
@@ -1,11 +1,6 @@
 <template>
-<div style="display: flex;flex-direction: column;justify-content: center;
-align-items: center; text-align: center;">
-    
-    <h1>
-        Ainda não tenho certeza de qual conteúdo colocar, então... ❤️
-    </h1>
-    <img src="/images/微信图片_20241025225635.jpg" alt="" style="width: 300px;">
+<div>
+    Com tudo isso, como eu poderia querer te machucar de propósito? Antes, eu briguei com um bom amigo, ele me excluiu e eu não sabia. Só percebi quando ele me adicionou de volta, e então eu soube que tinha sido abandonado. Eu sou assim, um pouco ingênuo. Eu dependo de quem é bom comigo, mas são poucas pessoas. Eu não tenho amigos e não socializo há muito tempo, porque meus pais não queriam que eu saísse para socializar. Então, eu valorizo muito nossa amizade. Eu queria escrever isso tudo porque estou muito triste. Você já me fez sentir muito calor humano e eu também espero ser mais independente para enfrentar meus próprios problemas, sem te fazer sentir a pressão. Eu sei que você está chateada agora, talvez eu não tenha feito o suficiente e te decepcionado.
 </div>
 </template>
 
