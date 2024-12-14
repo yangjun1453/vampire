9da7c7db07097ba5a0a1f6c96edc4a84f81f6a46
BaiduSucks <35864302+yangjun1453@users.noreply.github.com>
2024-12-14 11:56:03 +0800

    Update Page1.vue
    update

--- src/pages/Page1.vue
+++ src/pages/Page1.vue
@@ -10,7 +10,7 @@
 <script setup name="Page1" >
 import { ref } from 'vue';
 const encouragementMessages = [
-  'Eu tenho algumas coisas que gostaria de te dizer e espero que você possa ouvir o que está no meu coração. Se algo não estiver certo, por favor, me avise. Eu acho que você está absolutamente certa. Eu não estou bem, na verdade, sou ainda mais fechado. Eu raramente compartilho minhas experiências com você, certo? Sempre te disse que minha família parece ser boa, mas na verdade, eu só queria te dar energia positiva. Eu venho do interior. Meu pai tem hepatite B, que não tem cura, e minha família é muito pobre. Quando éramos crianças, morávamos em uma garagem. Eu tinha que ir ao banheiro na rua, e a única vez que pude usar um banheiro adequado foi no KFC. Eu me sinto muito inferior. A razão pela qual eu digo que sou independente emocionalmente é por causa da falta de emoções. Minha saúde mental não está boa, eu me fechei dentro de mim. Clara, você está certa em tudo, exceto por uma coisa: meu coração não é forte. '
+  '未完待续....我不知道小屋的主人是否会回来 '
 ];
 
 const randomIndex = Math.floor(Math.random() * encouragementMessages.length);
